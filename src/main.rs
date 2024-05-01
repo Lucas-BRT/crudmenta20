6f0fb960febb8d0cd5c4a52c820f2f63b377e7c3
lucasbrt-desktop <lucasbritodepaula.t@gmail.com>
2024-05-01 09:09:16 -0300

    feat: servindo a página public

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-#[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+use rocket::fs::FileServer;
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", FileServer::from("./public"))
 }
+
